4d3dbd2815ee57e0457764be7ace4ee81492d423
Artem Markelov <artem77timoha77@gmail.com>
2022-10-21 14:44:44 +0600

    Corrected the function so that the numbers do not repeat.

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,38 @@ import './App.css';
 import Number from "./Number/Number";
 
 function App() {
-  const massNumbers: number[] = [];
-  const printNumbers = () => {
-
-    do {
-      const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-      if (massNumbers[0] === undefined) {
-        massNumbers.push(randomNumber);
-      } else if (!massNumbers.includes(randomNumber)) {
-        massNumbers.push(randomNumber);
-      }
-
-    } while (massNumbers.length < 4)
-
-    massNumbers.sort((a, b)=> {
-      return a - b;
-    });
-
-    return massNumbers
-  }
-  console.log(massNumbers);
-
   const [number, setNumber] = useState([
-    {number: 0},
-    {number: 0},
-    {number: 0},
-    {number: 0},
-    {number: 0},
+    0, 0, 0, 0, 0
   ]);
 
-  const changeNumber = () => {
-    setNumber([
-      {number: printNumbers()[0]},
-      {number: printNumbers()[1]},
-      {number: printNumbers()[2]},
-      {number: printNumbers()[3]},
-      {number: printNumbers()[4]},
-    ]);
-  };
+  const printNumbers = () => {
+    const massNumbers: number[] = [];
+    do {
+      let randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+      if (massNumbers.length === 0) {
+        massNumbers.push(randomNumber);
+      } else if (massNumbers.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+      } else {
+        massNumbers.push(randomNumber);
+      }
+    } while (massNumbers.length <= 4)
+    massNumbers.sort((a, b) => {
+      return a - b;
+    });
+    setNumber(massNumbers);
+  }
 
   return (
     <div className="App">
       <div className="box">
-        <button onClick={changeNumber} className="btn">Get numbers</button>
+        <button onClick={printNumbers} className="btn">Get numbers</button>
         <div className="ball-box">
-          <Number number={number[0].number}></Number>
-          <Number number={number[1].number}></Number>
-          <Number number={number[2].number}></Number>
-          <Number number={number[3].number}></Number>
-          <Number number={number[4].number}></Number>
+          <Number number={number[0]}></Number>
+          <Number number={number[1]}></Number>
+          <Number number={number[2]}></Number>
+          <Number number={number[3]}></Number>
+          <Number number={number[4]}></Number>
         </div>
       </div>
     </div>
